921e78ef2e37d8ae68efc130a721826c27b3be43
Balavi1681 <vimi168135@gmail.com>
2022-06-09 11:56:03 +0530

    pie chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Highcharts from "highcharts/highstock";
+import PieChart from "highcharts-react-official";
 
-function App() {
+const options = {
+  chart: {
+    plotBackgroundColor: null,
+       plotBorderWidth: null,
+      plotShadow: false,
+    type: "pie"
+  },
+  title: {
+    text: "Browser market shares in January, 2018"
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+},
+  credits: {
+    enabled: false
+  },
+  accessibility: {
+    point: {
+        valueSuffix: '%'
+    }
+},
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: "pointer",
+      dataLabels: {
+        enabled: true,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+      },
+      showInLegend: false
+    }
+  },
+  series: [{
+    name: 'Brands',
+    colorByPoint: true,
+    data: [{
+        name: 'Chrome',
+        y: 61.41,
+        sliced: true,
+        selected: true
+    }, {
+        name: 'Internet Explorer',
+        y: 11.84
+    }, {
+        name: 'Firefox',
+        y: 10.85
+    }, {
+        name: 'Edge',
+        y: 4.67
+    }, {
+        name: 'Safari',
+        y: 4.18
+    }, {
+        name: 'Sogou Explorer',
+        y: 1.64
+    }, {
+        name: 'Opera',
+        y: 1.6
+    }, {
+        name: 'QQ',
+        y: 1.2
+    }, {
+        name: 'Other',
+        y: 2.61
+    }]
+}]
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>PIE CHART</h1>
+      <PieChart highcharts={Highcharts} options={options} />
     </div>
   );
-}
+};
 
 export default App;
